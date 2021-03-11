42691dadf7bad158215e91f7fdbd0e4c1bcba83f
Aleksandr Momziakov <73888668+Sasanano@users.noreply.github.com>
2021-03-11 17:33:03 +0300

    Form 1.0

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,7 +1,19 @@
-import {Component} from '../core/component'
+import { Component } from '../core/component'
+import { Form } from '../core/form'
 
 export class CreateComponent extends Component {
     constructor(id) {
         super(id)
+        
+        this.form = null
     }
+
+    init() {
+        this.$el.addEventListener('submit', submitHandler.bind(this))
+    }
+}
+
+function submitHandler(event) {
+    event.preventDefault()
+
 }
